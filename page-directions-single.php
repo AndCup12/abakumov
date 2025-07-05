d97dee2d3c5fb848b4592241fa0a4160dca37932
Arsen Asanov <tatarin08082002@gmail.com>
2025-07-05 23:14:30 +0300

    Добавил шаблое сингловой страницы направлений

---
+++ page-directions-single.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Template Name: Направления-single
+ * http://dontforget.pro
+ * @package WordPress
+ * @subpackage clean
+ */
+get_header();
+?>
+
+<main class="main" id="directions-single">
+</main>
+
+<?php get_footer(); ?>
