6ec938d6d15285527ed9dbeafc9167e953f23030
Arsen Asanov <tatarin08082002@gmail.com>
2025-07-13 11:13:35 +0300

    добавил страницу услуг

---
+++ page-services.php
@@ -0,0 +1,227 @@
+<?php
+get_header();
+?>
+
+<main class="main" id="services-main">
+  <section class="front-block _image-wrapper _section-lg">
+    <img class="front-block__logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/abacumov-front.svg" alt="abacumov">
+    <div class="container relative">
+      <div class="front-block__inner">
+        <div class="front-block__left">
+          <h1 class="front-block__title _title">
+            Услуги в Abakumov Clinic
+          </h1>
+          <div class="cursor">
+            <?php include(get_template_directory() . '/assets/images/icons/cursor.svg'); ?>
+          </div>
+          <div class="front-block__services">
+            <a class="front-block__service" href="#">
+              <div class="front-block__service-wrapper">
+                <img class="front-block__service-img _img" src="<?php echo get_template_directory_uri(); ?>/assets/images/front-block-10.webp" alt="front-block-10">
+              </div>
+              <div class="front-block__service-info">
+                <h4 class="front-block__service-title">
+                  Дневной стационар
+                </h4>
+                <div class="front-block__service-btn _gray-btn">
+                  <span>Узнать подробнее</span>
+                  <?php include(get_template_directory() . '/assets/images/icons/arrow-link.svg'); ?>
+                </div>
+              </div>
+            </a>
+            <a class="front-block__service" href="#">
+              <div class="front-block__service-wrapper">
+                <img class="front-block__service-img _img" src="<?php echo get_template_directory_uri(); ?>/assets/images/front-block-11.webp" alt="front-block-11">
+              </div>
+              <div class="front-block__service-info">
+                <h4 class="front-block__service-title">
+                  Ответы на часто
+                  задаваемые вопросы
+                </h4>
+                <div class="front-block__service-btn _gray-btn">
+                  <span>Узнать подробнее</span>
+                  <?php include(get_template_directory() . '/assets/images/icons/arrow-link.svg'); ?>
+                </div>
+              </div>
+            </a>
+            <a class="front-block__service" href="#">
+              <div class="front-block__service-wrapper">
+                <img class="front-block__service-img _img" src="<?php echo get_template_directory_uri(); ?>/assets/images/front-block-12.webp" alt="front-block-12">
+              </div>
+              <div class="front-block__service-info">
+                <h4 class="front-block__service-title">
+                  Медицинские
+                  услуги на дому
+                </h4>
+                <div class="front-block__service-btn _gray-btn">
+                  <span>Узнать подробнее</span>
+                  <?php include(get_template_directory() . '/assets/images/icons/arrow-link.svg'); ?>
+                </div>
+              </div>
+            </a>
+            <a class="front-block__service" href="#">
+              <div class="front-block__service-wrapper">
+                <img class="front-block__service-img _img" src="<?php echo get_template_directory_uri(); ?>/assets/images/front-block-13.webp" alt="front-block-13">
+              </div>
+              <div class="front-block__service-info">
+                <h4 class="front-block__service-title">
+                  Мед-бот диагностики
+                </h4>
+                <div class="front-block__service-btn _gray-btn">
+                  <span>Узнать подробнее</span>
+                  <?php include(get_template_directory() . '/assets/images/icons/arrow-link.svg'); ?>
+                </div>
+              </div>
+            </a>
+            <a class="front-block__service" href="#">
+              <div class="front-block__service-wrapper">
+                <img class="front-block__service-img _img" src="<?php echo get_template_directory_uri(); ?>/assets/images/front-block-14.webp" alt="front-block-14">
+              </div>
+              <div class="front-block__service-info">
+                <h4 class="front-block__service-title">
+                  Онлайн-консультация
+                </h4>
+                <div class="front-block__service-btn _gray-btn">
+                  <span>Узнать подробнее</span>
+                  <?php include(get_template_directory() . '/assets/images/icons/arrow-link.svg'); ?>
+                </div>
+              </div>
+            </a>
+            <a class="front-block__service" href="#">
+              <div class="front-block__service-wrapper">
+                <img class="front-block__service-img _img" src="<?php echo get_template_directory_uri(); ?>/assets/images/front-block-15.webp" alt="front-block-15">
+              </div>
+              <div class="front-block__service-info">
+                <h4 class="front-block__service-title">
+                  Удалённая
+                  поддержка врача
+                </h4>
+                <div class="front-block__service-btn _gray-btn">
+                  <span>Узнать подробнее</span>
+                  <?php include(get_template_directory() . '/assets/images/icons/arrow-link.svg'); ?>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Контакты -->
+  <section class="contacts">
+    <div class="container">
+      <h2 class="contacts__title _title">
+        Ждем вас ежедневно <br>с 8.00 до 20.00
+      </h2>
+
+      <div class="contacts__inner">
+        <div class="contacts__left">
+          <div class="contacts__left-wrapper">
+            <img class="contacts__left-img _img" src="<?php echo get_template_directory_uri(); ?>/assets/images/contacts-1.webp" alt="contacts-1">
+          </div>
+        </div>
+        <div class="contacts__right">
+          <div class="contacts__items">
+            <div class="contacts__item">
+              <div class="contacts__item-icon">
+                <?php include(get_template_directory() . '/assets/images/icons/contacts-1.svg'); ?>
+              </div>
+              <p class="contacts__item-title">
+                <span>Адрес:</span> г. Москва,<br>
+                ул. 1-я Бородинская д.3
+              </p>
+            </div>
+            <div class="contacts__item">
+              <div class="contacts__item-icon">
+                <?php include(get_template_directory() . '/assets/images/icons/contacts-2.svg'); ?>
+              </div>
+              <p class="contacts__item-title">
+                <span>Бесплатная</span><br>
+                парковка
+              </p>
+            </div>
+            <div class="contacts__item">
+              <div class="contacts__item-icon">
+                <?php include(get_template_directory() . '/assets/images/icons/contacts-3.svg'); ?>
+              </div>
+              <p class="contacts__item-title">
+                <span>Прием гл. врачом:</span><br> каждый четверг с 15.00<br>
+                до 17.00
+              </p>
+            </div>
+          </div>
+          <div class="contacts__info">
+            <p class="contacts__info-title">
+              Запись на прием:
+            </p>
+            <a class="contacts__info-link" href=tel:+74958431914">
+              +7 (495) 843-19-14
+            </a>
+            <div class="contacts__info-socials">
+              <a class="contacts__info-social" href="#">
+                <div class="contacts__info-icon contacts__info-icon--whatsapp">
+                  <?php include(get_template_directory() . '/assets/images/icons/whatsapp.svg'); ?>
+                </div>
+                <p class="contacts__info-text">
+                  Запись через<br>
+                  WhatsApp
+                </p>
+              </a>
+              <a class="contacts__info-social" href="#">
+                <div class="contacts__info-icon contacts__info-icon--telegram">
+                  <?php include(get_template_directory() . '/assets/images/icons/telegram.svg'); ?>
+                </div>
+                <p class="contacts__info-text">
+                  Запись через<br>
+                  Telegram
+                </p>
+              </a>
+            </div>
+            <a class="contacts__info-btn _gray-btn" href="#" data-fancybox>
+              Задать вопрос
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Карта -->
+  <section class="map-block">
+    <div class="container">
+      <h2 class="map-block__title _title">
+        Получите схемы проезда<br>
+        и все контакты себе клиники в WhatsApp
+      </h2>
+    </div>
+    <div class="map-block__inner relative _section-lg">
+      <div class="map-block__map" id="map"></div>
+      <div class="container">
+        <form class="map-block__form form-submit" method="POST" action="#" enctype="multipart/form-data">
+          <input type="hidden" name="from" class="from" value="Закажите обратный звонок">
+          <input type="hidden" name="newToken" value="<?php echo (rand(10000, 99999)) ?>">
+          <h4 class="map-block__form-title _title">
+            Заполните форму ниже
+          </h4>
+          <p class="map-block__form-subtitle">
+            чтобы получить подробную <br>схему проезда 
+          </p>
+          <div class="map-block__form-inner">
+            <input class="_input map-block__form-inp" type="tel" name="phone" placeholder="Введите номер телефона">
+            <button class="map-block__form-btn _gray-btn" type="submit">
+              Получите схемы проезда<br>
+              и все контакты в WhatsApp
+            </button>
+          </div>
+          <p class="map-block__form-politic politic-text">
+            Нажимая на кнопку, вы соглашаетесь с <a href="#politics" data-fancybox>
+              Политикой конфиденциальности
+            </a>
+          </p>
+        </form>
+
+      </div>
+    </div>
+  </section>
+</main>
+
+<?php get_footer(); ?>
